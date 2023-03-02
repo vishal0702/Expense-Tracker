2e7c82493f09a767299d35ce8c6e13cf6f1d0ac7
Vishal Srivastava <vishalsrivi123@gmail.com>
2023-03-02 16:11:41 +0530

    Replacing functions with Arrow functions

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Expenses from "./components/Expenses/Expenses";
 import "./App.css";
 import React from "react";
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: "e1",
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,7 +3,7 @@ import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
-function ExpenseItem(props) {
+const  ExpenseItem = (props) => {
 
     // const expenseDate= new Date(2023, 1, 27);
     // const expenseTitle= 'Car Insurance';

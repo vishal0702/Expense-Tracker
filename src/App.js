d0905edc9dfb2ffc3918a8131a327067c776584c
Vishal Srivastava <vishalsrivi123@gmail.com>
2023-03-02 15:59:12 +0530

    importing React

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@
 import Expenses from "./components/Expenses";
 
 import "./App.css";
+import React from "react";
 
 function App() {
   const expenses = [
     {
       id: "e1",
       title: "Toilet Paper",
-      amount: 94.12,
+      amount: 194.12,
       date: new Date(2022, 7, 14),
     },
     { id: "e2", title: "New TV", amount: 799.49, date: new Date(2023, 2, 12) },
@@ -22,41 +23,26 @@ function App() {
     {
       id: "e4",
       title: "New Desk (Wooden)",
-      amount: 450,
+      amount: 450.01,
       date: new Date(2022, 5, 12),
     },
   ];
 
+  //JSX Code
   return (
     <div>
       <h2>Let's get started!</h2>
-      {/* <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      /> */}
-
       <Expenses items={expenses} />
     </div>
   );
+
+  //alternative of above return block
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses,{items:expenses})
+  // );
 }
 
 export default App;

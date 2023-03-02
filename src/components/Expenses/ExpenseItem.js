54005fecd9c08791a9f1a59905aae5099b78464c
Vishal Srivastava <vishalsrivi123@gmail.com>
2023-03-02 18:13:32 +0530

    Introducing state

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
@@ -8,15 +8,23 @@ const  ExpenseItem = (props) => {
     // const expenseDate= new Date(2023, 1, 27);
     // const expenseTitle= 'Car Insurance';
     // const expensePrice= 359.46;
+  
+  const [title, setTitle] = useState(props.title);
+
+  // let title = props.title;
+  const clickHandler= () => {
+    setTitle('Updated!');
+  };
 
   return (
     <Card className="expense-item">
       
       <div className='expense-item__description'>
         <ExpenseDate date={props.date} />
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
